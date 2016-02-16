088ca988a644d4856f2ab316e7100ca4d304e4ac
atanas-bg <atanas.v.atanasov@gmail.com>
2016-02-16 15:03:03 +0200

    Advanced C# HomeWork

---
+++ AdnacedC#/ArraysListsStacksQueues/03.FindMinMaxAverge/FindMinMaxAverge.cs
@@ -0,0 +1,88 @@
+﻿namespace _03.FindMinMaxAverge
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using Microsoft.SqlServer.Server;
+
+    class FindMinMaxAverge
+    {
+        static void Main()
+        {
+            var inputNumbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var maxIndex = inputNumbers.Length;
+            var intList = new List<int>();
+            var doubleList = new List<double>();
+
+            for (int currentIndex = 0; currentIndex < maxIndex; currentIndex++)
+            {
+                int intNumber;
+                bool isInt = int.TryParse(
+                       inputNumbers[currentIndex],
+                       NumberStyles.Any,
+                       CultureInfo.InvariantCulture,
+                       out intNumber);
+
+                if (isInt)
+                {
+                    intList.Add(intNumber);
+                }
+                else
+                {
+                    double doubleNumber;
+                    bool isDouble = double.TryParse(
+                        inputNumbers[currentIndex],
+                        NumberStyles.Any,
+                        CultureInfo.InvariantCulture,
+                        out doubleNumber);
+                    if (isDouble)
+                    {
+                        if (doubleNumber != (int)doubleNumber)
+                        {
+                            doubleList.Add(doubleNumber);
+                        }
+                        else
+                        {
+                            intList.Add((int)doubleNumber);
+                        }
+                    }
+                }
+            }
+            
+            string intResult = IntFormatResult(intList);
+            string doubleResult = DoubleFormatResult(doubleList);
+
+            Console.WriteLine(doubleResult);
+            Console.WriteLine();
+            Console.WriteLine(intResult);
+        }
+
+        private static string IntFormatResult(List<int> intList)
+        {
+            string outputIntList = string.Join(", ", intList);
+            int minNumberIntList = intList.Min();
+            int maxNumberIntList = intList.Max();
+            int sumIntList = intList.Sum();
+            double averageIntList = intList.Average();
+            string intResult = string.Format("[{0}] -> min: {1}, max: {2}, sum: {3}, average: {4:F2}", outputIntList,
+                minNumberIntList, maxNumberIntList, sumIntList, averageIntList);
+
+            return intResult;
+        }
+
+        private static string DoubleFormatResult(List<double> doubleList)
+        {
+            string outputDoubleList = string.Join(", ", doubleList);
+            double minNumberDoubleList = doubleList.Min();
+            double maxNumberDoubleList = doubleList.Max();
+            double sumDoubleList = doubleList.Sum();
+            double averageDoubleList = doubleList.Average();
+            string doubleResult = string.Format("[{0}] -> min: {1}, max: {2}, sum: {3}, average: {4:F2}", outputDoubleList,
+                minNumberDoubleList, maxNumberDoubleList, sumDoubleList, averageDoubleList);
+
+            return doubleResult;
+        }
+    }
+}
---
+++ AdnacedC#/ArraysListsStacksQueues/06.SubsetSum/SubsetSums.cs
@@ -0,0 +1,48 @@
+﻿namespace _06.SubsetSums
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class SubsetSums
+    {
+        static void Main()
+        {
+            int numberN = int.Parse(Console.ReadLine());
+            int[] inputIntArray = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Distinct()
+                .ToArray();
+            int inputIntArrayLength = inputIntArray.Length;
+            int masksCount = 1 << inputIntArrayLength;
+            var numbersList = new List<int>();
+            bool noSubsetsExist = true;
+
+            for (int currentMask = 0; currentMask < masksCount; currentMask++)
+            {
+                for (int index = 0; index < inputIntArrayLength; index++)
+                {
+                    if ((currentMask & (1 << index)) > 0)
+                    {
+                        numbersList.Add(inputIntArray[inputIntArrayLength - index - 1]);
+                    }
+                }
+
+                numbersList.Reverse();
+                if (numbersList.Count > 0 && numbersList.Sum() == numberN)
+                {
+                    Console.WriteLine("{0} = {1}", string.Join(" + ", numbersList), numberN);
+                    noSubsetsExist = false;
+                }
+
+                numbersList.Clear();
+            }
+
+            if (noSubsetsExist)
+            {
+                Console.WriteLine("No matching subsets.");
+            }
+        }
+    }
+}
